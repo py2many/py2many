15f1aa149c7d3c697daed161dab105c2512983b9
Lukas Martinelli <me@lukasmartinelli.ch>
2015-01-23 22:48:34 +0100

    Added cmd interface for transpiling

---
+++ py14.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import sys
+from transpiler import transpile
+
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    source = open(filename).read()
+#    import ipdb; ipdb.set_trace()
+    cpp = transpile(source)
+    print('#include <iostream>')
+    print('#include <string>')
+    print('#include <vector>')
+    print('#include <utility>')
+    print('#include "sys.h"')
+    print(cpp)
