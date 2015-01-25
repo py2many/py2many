11ebaed3ba2bba41edcc5e8e83cf4ad08011a113
Lukas Martinelli <me@lukasmartinelli.ch>
2015-01-25 10:38:47 +0100

    Added comb_sort and fib to test

--- py14/test_transpiler.py
+++ py14/test_transpiler.py
@@ -69,14 +69,16 @@ def test_map_function():
         "   return results",
     )
     cpp = transpile(source)
-    assert cpp == ("auto map = [](auto values, auto fun) {\n"
-                   "std::vector<decltype(fun(std::declval"
-                   "<typename decltype(values)::value_type>()))> results {};\n"
-                   "for(auto v : values) {\n"
-                   "results.push_back(fun(v));\n"
-                   "}\n"
-                   "return results;\n"
-                   "};")
+    assert cpp == (
+        "auto map = [](auto values, auto fun) {\n"
+        "std::vector<decltype(fun(std::declval"
+        "<typename decltype(values)::value_type>()))> results {};\n"
+        "for(auto v : values) {\n"
+        "results.push_back(fun(v));\n"
+        "}\n"
+        "return results;\n"
+        "};"
+    )
 
 
 def test_bubble_sort():
@@ -90,14 +92,78 @@ def test_bubble_sort():
         "    return seq",
     )
     cpp = transpile(source)
-    assert cpp == ("auto sort = [](auto seq) {\n"
-                   "auto L = py14::len(seq);\n"
-                   "for(auto _ : py14::range(L)) {\n"
-                   "for(auto n : py14::range(1, L)) {\n"
-                   "if(seq[n] < seq[n - 1]) {\n"
-                   "std::tie(seq[n - 1], seq[n]) = "
-                   "std::make_tuple(seq[n], seq[n - 1]);\n"
-                   "}\n"
-                   "}\n" "}\n"
-                   "return seq;\n"
-                   "};")
+    assert cpp == (
+        "auto sort = [](auto seq) {\n"
+        "auto L = py14::len(seq);\n"
+        "for(auto _ : py14::range(L)) {\n"
+        "for(auto n : py14::range(1, L)) {\n"
+        "if(seq[n] < seq[n - 1]) {\n"
+        "std::tie(seq[n - 1], seq[n]) = "
+        "std::make_tuple(seq[n], seq[n - 1]);\n"
+        "}\n"
+        "}\n" "}\n"
+        "return seq;\n"
+        "};"
+    )
+
+
+def test_fib():
+    source = parse(
+        "def fib(n):",
+        "    if n == 1:",
+        "        return 1",
+        "    elif n == 0:",
+        "        return 0",
+        "    else:",
+        "        return fib(n-1) + fib(n-2)",
+    )
+    cpp = transpile(source)
+    assert cpp == (
+        "template <typename T1>\n"
+        "auto fib(T1 n) {\n"
+        "if(n == 1) {\n"
+        "return 1;\n"
+        "} else {\n"
+        "if(n == 0) {\n"
+        "return 0;\n"
+        "} else {\n"
+        "return fib(n - 1) + fib(n - 2);\n"
+        "}\n"
+        "}\n"
+        "}"
+    )
+
+
+def test_comb_sort():
+    source = parse(
+        "def sort(seq):",
+        "    gap = len(seq)",
+        "    swap = True",
+        "    while gap > 1 or swap:",
+        "        gap = max(1, int(gap / 1.25))",
+        "        swap = False",
+        "        for i in range(len(seq) - gap):",
+        "            if seq[i] > seq[i + gap]:",
+        "                seq[i], seq[i + gap] = seq[i + gap], seq[i]",
+        "                swap = True",
+        "                return seq",
+    )
+    cpp = transpile(source)
+    assert cpp == (
+        "auto sort = [](auto seq) {\n"
+        "auto gap = py14::len(seq);\n"
+        "auto swap = true;\n"
+        "while (gap > 1||swap) {\n"
+        "gap = std::max(1, py14::to_int(gap / 1.25));\n"
+        "swap = false;\n"
+        "for(auto i : py14::range(py14::len(seq) - gap)) {\n"
+        "if(seq[i] > seq[i + gap]) {\n"
+        "std::tie(seq[i], seq[i + gap]) = "
+        "std::make_tuple(seq[i + gap], seq[i]);\n"
+        "swap = true;\n"
+        "return seq;\n"
+        "}\n"
+        "}\n"
+        "}\n"
+        "};"
+    )
