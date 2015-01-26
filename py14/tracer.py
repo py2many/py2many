c7776ebb35139de1ecce28859bea9dff3b940212
Lukas Martinelli <me@lukasmartinelli.ch>
2015-01-26 08:45:29 +0100

    Refactored tracer into visitor

--- py14/tracer.py
+++ py14/tracer.py
@@ -2,9 +2,7 @@
 Trace object types that are inserted into Python list.
 """
 import ast
-from context import add_variable_context
 from clike import CLikeTranspiler
-from generic.multidispatch import multifunction
 
 
 def decltype(node):
@@ -27,53 +25,21 @@ def is_list(node):
         return is_list(node.value)
     elif isinstance(node, ast.Name):
         var = node.scopes.find(node.id)
-        return hasattr(var, "assigned_from") and \
-               is_list(var.assigned_from.value)
+        return (hasattr(var, "assigned_from") and
+                is_list(var.assigned_from.value))
     else:
         return False
 
 
-@multifunction(ast.Num)
 def value_expr(node):
     """
     Follow all assignments down the rabbit hole in order to find
     the value expression of a name.
     The boundary is set to the current scope.
     """
-    return str(node.n)
-
-@value_expr.when(ast.Str)
-def value_expr(node):
-    return node.s
-
-@value_expr.when(ast.Name)
-def value_expr(node):
-    var = node.scopes.find(node.id)
-    if isinstance(var.assigned_from, ast.For):
-        iter = var.assigned_from.iter
-        return "std::declval<typename decltype({0})::value_type>()".format(value_expr(iter))
-    elif isinstance(var.assigned_from, ast.FunctionDef):
-        return var.id
-    else:
-        return value_expr(var.assigned_from.value)
-
-@value_expr.when(ast.Call)
-def value_expr(node):
-    params = ",".join([value_expr(arg) for arg in node.args])
-    return "{0}({1})".format(node.func.id, params)
-
-@value_expr.when(ast.Assign)
-def value_expr(node):
-        return value_expr(node.value)
-
-@value_expr.when(ast.BinOp)
-def value_expr(node):
-    return "{0} {1} {2}".format(value_expr(node.left),
-                                CLikeTranspiler().visit(node.op),
-                                value_expr(node.right))
+    return ValueExpressionVisitor().visit(node)
 
 
-@multifunction(ast.Num)
 def value_type(node):
     """
     Guess the value type of a node based on the manipulations or assignments
@@ -81,42 +47,70 @@ def value_type(node):
     Special case: If node is a container like a list the value type inside the
     list is returned not the list type itself.
     """
-    return value_expr(node)
+    return ValueTypeVisitor().visit(node)
 
 
-@value_type.when(ast.Str)
-def value_type(node):
-    return value_expr(node)
+class ValueExpressionVisitor(ast.NodeVisitor):
+    def visit_Num(self, node):
+        return str(node.n)
 
+    def visit_Str(self, node):
+        return node.s
 
-@value_type.when(ast.Assign)
-def value_type(node):
-    if isinstance(node.value, ast.List):
-        if len(node.value.elts) > 0:
-            val = node.value.elts[0]
-            return value_type(val)
+    def visit_Name(self, node):
+        var = node.scopes.find(node.id)
+        if isinstance(var.assigned_from, ast.For):
+            iter = var.assigned_from.iter
+            return "std::declval<typename decltype({0})::value_type>()".format(
+                   self.visit(iter))
+        elif isinstance(var.assigned_from, ast.FunctionDef):
+            return var.id
         else:
-            target = node.targets[0]
-            var = node.scopes.find(target.id)
-            first_added_value = var.calls[0].args[0]
-            return value_expr(first_added_value)
-    else:
-        return value_type(node.value)
+            return self.visit(var.assigned_from.value)
+
+    def visit_Call(self, node):
+        params = ",".join([self.visit(arg) for arg in node.args])
+        return "{0}({1})".format(node.func.id, params)
+
+    def visit_Assign(self, node):
+        return self.visit(node.value)
+
+    def visit_BinOp(self, node):
+        return "{0} {1} {2}".format(self.visit(node.left),
+                                    CLikeTranspiler().visit(node.op),
+                                    self.visit(node.right))
 
 
-@value_type.when(ast.Name)
-def value_type(node):
-    var = node.scopes.find(node.id)
-    if defined_before(var, node):
-        return node.id
-    else:
-        return value_type(var.assigned_from.value)
+class ValueTypeVisitor(ast.NodeVisitor):
+    def visit_Num(self, node):
+        return value_expr(node)
 
+    def visit_Str(self, node):
+        return value_expr(node)
 
-@value_type.when(ast.Call)
-def value_type(node):
-    params = ",".join([value_type(arg) for arg in node.args])
-    return "{0}({1})".format(node.func.id, params)
+    def visit_Name(self, node):
+        var = node.scopes.find(node.id)
+        if defined_before(var, node):
+            return node.id
+        else:
+            return self.visit(var.assigned_from.value)
+
+    def visit_Call(self, node):
+        params = ",".join([self.visit(arg) for arg in node.args])
+        return "{0}({1})".format(node.func.id, params)
+
+    def visit_Assign(self, node):
+        if isinstance(node.value, ast.List):
+            if len(node.value.elts) > 0:
+                val = node.value.elts[0]
+                return self.visit(val)
+            else:
+                target = node.targets[0]
+                var = node.scopes.find(target.id)
+                first_added_value = var.calls[0].args[0]
+                return value_expr(first_added_value)
+        else:
+            return self.visit(node.value)
 
 
 def defined_before(node1, node2):
