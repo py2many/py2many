c36e05bd20415dd998d22c2af6ea942b9ba3946a
Lukas Martinelli <me@lukasmartinelli.ch>
2015-11-14 12:01:39 +0100

    Use clang format

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+import logging
+import sys
+from subprocess import Popen, PIPE, STDOUT
+
 from py14.transpiler import transpile
 from flask import Flask, request, redirect, url_for, send_from_directory
 
 
 app = Flask(__name__)
-app.debug = True
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.DEBUG)
 
 
 @app.route('/')
@@ -17,5 +22,19 @@ def static_proxy(path):
     return app.send_static_file(path)
 
 
+@app.route('/transpile', methods=['POST'])
+def transpilation():
+    source = request.data
+    cpp = transpile(source, headers=True, testing=False)
+    return format_cpp(cpp)
+
+
+def format_cpp(cpp):
+    proc = Popen(['clang-format'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    formatted_cpp = proc.communicate(input=cpp)[0]
+    proc.wait()
+    return formatted_cpp
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
