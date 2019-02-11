faeb4ebc39213cb64ab79e050a1de0c74e12173d
Gert Hulselmans <gert.hulselmans@kuleuven.vib.be>
2019-02-11 18:37:16 +0100

    Add basic support for "with", so code inside the with block can be transpiled.
    Add basic support for "with", so code inside the "with" block can be transpiled
    while the "with" statement itself is still unimplemented and no traceback is
    generated when encountering a "with" block (AttributeError: 'list' object has
    no attribute '_fields').

--- pyrs/transpiler.py
+++ pyrs/transpiler.py
@@ -419,7 +419,24 @@ class RustTranspiler(CLikeTranspiler):
         return "raise!({0}); //unsupported".format(self.visit(node.exc))
 
     def visit_With(self, node):
-        return "with!({0}); //unsupported".format(self.visit(node.body))
+        buf = []
+
+        with_statement = "with!("
+        for i in node.items:
+            if i.optional_vars:
+                with_statement += "{0} as {1}, ".format(self.visit(i.context_expr),
+                                                        self.visit(i.optional_vars))
+            else:
+                with_statement += "{0}, ".format(self.visit(i.context_expr))
+        with_statement = with_statement[:-2] + ") { //unsupported"
+        buf.append(with_statement)
+
+        for n in node.body:
+            buf.append(self.visit(n))
+
+        buf.append('}')
+
+        return "\n".join(buf)
 
     def visit_Await(self, node):
         return "await!({0})".format(self.visit(node.value))
