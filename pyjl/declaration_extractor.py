fdb19562320792c3b2c73c6c23c34e9a6584e08f
Arun Sharma <arun@sharma-home.net>
2021-01-27 14:35:38 -0800

    Julia: Add missing file

---
+++ pyjl/declaration_extractor.py
@@ -0,0 +1,10 @@
+from common.declaration_extractor import (
+    DeclarationExtractor as CommonDeclarationExtractor,
+)
+from .clike import julia_type_map
+
+
+class DeclarationExtractor(CommonDeclarationExtractor):
+    def __init__(self, transpiler):
+        super().__init__(transpiler)
+        self._type_map = julia_type_map
