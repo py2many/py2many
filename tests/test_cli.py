9143c5b140bc12daa9e13ab0111cfc58eb06e75f
John Vandenberg <jayvdb@gmail.com>
2021-04-11 02:23:45 +0800

    tests: Run compile on generated files
    Also moves infer-ops to infer_ops as - is not allowed
    for some languages.
    
    And update .gitignore with all the build artefacts.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,6 +3,7 @@ import unittest
 import sys
 from distutils import spawn
 from pathlib import Path
+from subprocess import run
 from unittest.mock import Mock
 
 from unittest_expander import foreach, expand
@@ -10,11 +11,40 @@ from unittest_expander import foreach, expand
 from py2many.cli import main, _get_all_settings
 
 KEEP_GENERATED = os.environ.get("KEEP_GENERATED", False)
+COMPILERS = {
+    # cpp is disabled due to https://github.com/adsharma/py2many/issues/24
+    # "cpp": ["clang", "-std=c++14"],
+    "dart": ["dart"],
+    "go": ["go", "tool", "compile"],
+    "julia": ["julia"],
+    "kotlin": ["kotlinc"],
+    "nim": ["nim", "compile", "--nimcache:."],
+    "rust": ["rust-script"],
+}
+EXPECTED_COMPILE_FAILURES = [
+    "binit.go",  # https://github.com/adsharma/py2many/issues/23
+    "binit.kt",  # https://github.com/adsharma/py2many/issues/28
+    "binit.nim",  # https://github.com/adsharma/py2many/issues/19
+    "binit.rs",  # https://github.com/adsharma/py2many/issues/19
+    "infer.go",  # https://github.com/adsharma/py2many/issues/23
+    "infer.kt",  # https://github.com/adsharma/py2many/issues/28
+    "infer_ops.go",  # https://github.com/adsharma/py2many/issues/16
+    "infer_ops.kt",  # https://github.com/adsharma/py2many/issues/28
+    "infer_ops.nim",  # https://github.com/adsharma/py2many/issues/16
+    "infer_ops.rs",  # https://github.com/adsharma/py2many/issues/16
+    "int_enum.jl",  # https://github.com/adsharma/py2many/issues/26
+    "int_enum.kt",  # https://github.com/adsharma/py2many/issues/28
+    "lambda.dart",  # https://github.com/adsharma/py2many/issues/34
+    "lambda.go",  # https://github.com/adsharma/py2many/issues/15
+    "lambda.kt",  # https://github.com/adsharma/py2many/issues/28
+    "lambda.nim",  # https://github.com/adsharma/py2many/issues/27
+    "lambda.rs",  # https://github.com/adsharma/py2many/issues/15
+]
 
 
 @expand
 class CodeGeneratorTests(unittest.TestCase):
-    TEST_CASES = ["fib", "rect", "infer", "infer-ops", "binit", "lambda", "int_enum"]
+    TEST_CASES = ["fib", "rect", "infer", "infer_ops", "binit", "lambda", "int_enum"]
     TESTS_DIR = Path(__file__).parent
     SETTINGS = _get_all_settings(Mock(indent=4))
     maxDiff = None
@@ -39,6 +69,18 @@ class CodeGeneratorTests(unittest.TestCase):
                 if os.path.exists(f"expected/{case}{ext}"):
                     with open(f"expected/{case}{ext}") as f2:
                         self.assertEqual(f2.read(), generated)
+
+            if ext == ".dart" and "void main() {" not in generated and "main() {" not in generated:
+                # See https://github.com/adsharma/py2many/issues/25
+                raise unittest.SkipTest(f"{case}{ext} doesnt have a main")
+            expect_failure = f"{case}{ext}" in EXPECTED_COMPILE_FAILURES
+            compiler = COMPILERS.get(lang)
+            if compiler and spawn.find_executable(compiler[0]):
+                proc = run([*compiler, f"cases/{case}{ext}"], check=not expect_failure)
+
+                assert not expect_failure or proc.returncode != 0
+                if proc.returncode:
+                    raise unittest.SkipTest(f"{case}{ext} doesnt compile")
 
                 if not os.path.exists(f"expected/{case}{ext}"):
                     with open(f"expected/{case}{ext}", "w") as f:
