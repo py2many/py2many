cf0bcd9fedc92b776c5131767b031939248182ba
Arun Sharma <arun@sharma-home.net>
2021-04-16 09:06:31 -0700

    julia: unmask test failures
    Add a compiler, so we can detect out of sync expected outputs

--- tests/expected/fib.jl
+++ tests/expected/fib.jl
@@ -4,3 +4,8 @@ function fib(i::Int64)::Int64
     end
     return (fib((i - 1)) + fib((i - 2)))
 end
+
+function main()
+    rv = fib(5)
+    println(join([rv], " "))
+end
--- tests/expected/print.jl
+++ tests/expected/print.jl
@@ -1,5 +1,9 @@
-function main()
+function show()
     println(join([2], " "))
     println(join(["b"], " "))
     println(join([2, "b"], " "))
+end
+
+function main()
+    show()
 end
