b041f7b2a8ff42cc1cc1b4101a6fb33c3ec109b2
Arun Sharma <arun@sharma-home.net>
2021-04-20 10:52:03 -0700

    nim: fix for loop ranges

--- pynim/transpiler.py
+++ pynim/transpiler.py
@@ -124,11 +124,11 @@ class NimTranspiler(CLikeTranspiler):
 
     def visit_range(self, node, vargs: List[str]) -> str:
         if len(node.args) == 1:
-            return "0..{}".format(vargs[0])
+            return f"(0..{vargs[0]} - 1)"
         elif len(node.args) == 2:
-            return "{}..{}".format(vargs[0], vargs[1])
+            return f"({vargs[0]}..{vargs[1]} - 1)"
         elif len(node.args) == 3:
-            return "countup({}, {}, {})".format(vargs[0], vargs[1], vargs[2])
+            return f"countup({vargs[0]}, {vargs[1]} - 1, {vargs[2]})"
 
         raise Exception(
             "encountered range() call with unknown parameters: range({})".format(vargs)
