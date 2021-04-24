7ab99eed9bcf798f18fc0bc34a9d8d08a5ef9037
Arun Sharma <arun@sharma-home.net>
2021-04-23 23:52:39 -0700

    dart: fix the lamda test case
    Fixes https://github.com/adsharma/py2many/issues/34

--- tests/expected/lambda.dart
+++ tests/expected/lambda.dart
@@ -1,12 +1,11 @@
 // @dart=2.9
 import 'package:sprintf/sprintf.dart';
 
- show() {
-Callable myfunc = { x, y -> (x + y) };
-print(sprintf("%s", [myfunc(1, 2)]));}
-
-
-void main() {
-show();
+show() {
+  var myfunc = (x, y) => (x + y);
+  print(sprintf("%s", [myfunc(1, 2)]));
 }
 
+void main() {
+  show();
+}
