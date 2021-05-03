cfadbd95bcacfec636072dedb2e47b9f4297434b
Arun Sharma <arun@sharma-home.net>
2021-05-03 10:24:48 -0700

    go: add missing str_enum.go

---
+++ tests/expected/str_enum.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Colors string
+
+const (
+	RED   Colors = "red"
+	GREEN        = "green"
+	BLUE         = "blue"
+)
+
+func show() {
+	color_map := map[Colors]string{RED: "1", GREEN: "2", BLUE: "3"}
+	var a Colors = GREEN
+	if a == GREEN {
+		fmt.Printf("%v\n", "green")
+	} else {
+		fmt.Printf("%v\n", "Not green")
+	}
+	fmt.Printf("%v\n", len(color_map))
+}
+
+func main() {
+	show()
+}
