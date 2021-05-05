8854321e21997bf0f35d3283be8c3e7ec9fc670a
John Vandenberg <jayvdb@gmail.com>
2021-05-05 16:19:05 +0800

    pyjl: Partially handle ''.format

--- pyjl/transpiler.py
+++ pyjl/transpiler.py
@@ -20,7 +20,11 @@ class JuliaMethodCallRewriter(ast.NodeTransformer):
                 new_func_name = "push!"
             else:
                 new_func_name = fname.attr
-            node.args = [ast.Name(id=fname.value.id, lineno=node.lineno)] + node.args
+            if get_id(fname.value):
+                node0 = ast.Name(id=get_id(fname.value), lineno=node.lineno)
+            else:
+                node0 = fname.value
+            node.args = [node0] + node.args
             node.func = ast.Name(id=new_func_name, lineno=node.lineno, ctx=fname.ctx)
         return node
 
