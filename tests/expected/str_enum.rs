a9ea9df83b8150a2de40413c50f813ab93233840
John Michael Burke <johnmichael.burke@swift-nav.com>
2021-05-07 20:29:59 -0700

    pyrs: Add EnumVariantNames to StrEnum

--- tests/expected/str_enum.rs
+++ tests/expected/str_enum.rs
@@ -9,9 +9,9 @@
 extern crate strum;
 extern crate strum_macros;
 use std::collections::HashMap;
-use strum_macros::{Display, EnumString};
+use strum_macros::{Display, EnumString, EnumVariantNames};
 
-#[derive(Clone, Debug, Display, EnumString, Eq, Hash, PartialEq)]
+#[derive(Clone, Debug, Display, EnumString, EnumVariantNames, Eq, Hash, PartialEq)]
 pub enum Colors {
     #[strum(serialize = "red")]
     RED,
