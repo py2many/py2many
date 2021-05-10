a7af7a560fd4794db12828b70acb592cc4326855
John Michael Burke <johnmichael.burke@swift-nav.com>
2021-05-10 15:20:53 -0700

    [Rust]Add allow for clippy::upper_case_acronyms.

--- tests/expected/global2.rs
+++ tests/expected/global2.rs
@@ -6,6 +6,8 @@
 //! lazy_static = "*"
 //! ```
 
+#![allow(clippy::upper_case_acronyms)]
+#![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 #![allow(unused_imports)]
