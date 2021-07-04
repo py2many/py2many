bccc8d81f5ffdaf7e30ae94710c270514452aebd
John Vandenberg <jayvdb@gmail.com>
2021-07-05 07:06:29 +0800

    Create custom exception AstUnrecognisedBinOp
    This exception occurs often when parsing Python code.
    Isolating it allows for exploration of the causes.

--- py2many/exceptions.py
+++ py2many/exceptions.py
@@ -12,6 +12,15 @@ class AstNotImplementedError(AstErrorBase, NotImplementedError):
     """Node is not supported by the transpiler"""
 
 
+class AstUnrecognisedBinOp(AstNotImplementedError):
+    """BinOp using unrecognised combination"""
+
+    def __init__(self, left_id: str, right_id: str, node: ast.AST):
+        self.lineno = node.lineno
+        self.col_offset = node.col_offset
+        super().__init__(f"{left_id} {type(node.op)} {right_id}", node)
+
+
 class AstIncompatibleAssign(AstErrorBase, TypeError):
     """Assignment target has type annotation that is incompatible with expression"""
 
