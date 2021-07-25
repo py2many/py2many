423e52de8ac4983ac49d66a2933e497ce719af81
div72 <60045611+div72@users.noreply.github.com>
2021-07-26 00:30:46 +0300

    basic for target inference

---
+++ tests/expected/bitops.v
@@ -0,0 +1,22 @@
+module main
+
+fn main_func() {
+	mut ands := []bool{}
+	mut ors := []bool{}
+	mut xors := []bool{}
+	for a in [false, true] {
+		for b in [false, true] {
+			ands << ((a && b))
+			ors << ((a || b))
+			xors << ((a != b))
+		}
+	}
+	assert ands == [false, false, false, true]
+	assert ors == [false, true, true, true]
+	assert xors == [false, true, true, false]
+	println('OK')
+}
+
+fn main() {
+	main_func()
+}
