d372000c0c717c250722a54a1ab577ed639c437e
MiguelMarcelino <mac.marcelino@outlook.pt>
2021-12-15 23:59:15 +0000

    Changes made to n-body problem

--- pyjl/tests/test_performance/n_body_problem_mod.jl
+++ pyjl/tests/test_performance/n_body_problem_mod.jl
@@ -14,34 +14,30 @@ end
 PI = 3.141592653589793
 SOLAR_MASS = 4*PI*PI
 DAYS_PER_YEAR = 365.24
-BODIES = Dict([
-    ("sun", ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS)),
-    ("jupiter", (
-        [4.841431442464721, -1.1603200440274284, -0.10362204447112311], 
+BODIES = [
+    # Sun
+    ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS)
+    # Jupiter
+    ([4.841431442464721, -1.1603200440274284, -0.10362204447112311], 
         [0.001660076642744037*DAYS_PER_YEAR, 0.007699011184197404*DAYS_PER_YEAR, -6.90460016972063e-05*DAYS_PER_YEAR],
         0.0009547919384243266*SOLAR_MASS)
-    ), 
-    ("saturn", (
-        [8.34336671824458, 4.124798564124305, -0.4035234171143214],
+    # Saturn
+    ([8.34336671824458, 4.124798564124305, -0.4035234171143214],
         [-0.002767425107268624*DAYS_PER_YEAR, 0.004998528012349172*DAYS_PER_YEAR, 2.3041729757376393e-05*DAYS_PER_YEAR], 
         0.0002858859806661308*SOLAR_MASS)
-    ), 
-    ("uranus", (
-        [12.894369562139131, -15.111151401698631, -0.22330757889265573],
+    # Uranus
+    ([12.894369562139131, -15.111151401698631, -0.22330757889265573],
         [0.002964601375647616*DAYS_PER_YEAR, 0.0023784717395948095*DAYS_PER_YEAR, -2.9658956854023756e-05*DAYS_PER_YEAR], 
         4.366244043351563e-05*SOLAR_MASS)
-    ), 
-    ("neptune", (
-        [15.379697114850917, -25.919314609987964, 0.17925877295037118], 
+    # Neptune
+    ([15.379697114850917, -25.919314609987964, 0.17925877295037118], 
         [0.0026806777249038932*DAYS_PER_YEAR, 0.001628241700382423*DAYS_PER_YEAR, -9.515922545197159e-05*DAYS_PER_YEAR], 
         5.1513890204661145e-05*SOLAR_MASS)
-    )
-])
-SYSTEM = collect(values(BODIES))
-PAIRS = combinations(SYSTEM)
+]
+PAIRS = combinations(BODIES)
 
-function advance(dt, n, bodies = SYSTEM, pairs = PAIRS)
-    for i in (0:n - 1)
+function advance(dt, n, bodies = BODIES, pairs = PAIRS)
+    for i in (1:n)
         for ((a1,v1,m1),(a2,v2,m2)) in pairs
             # (([x1, y1, z1], v1, m1), ([x2, y2, z2], v2, m2))
             # Converted
@@ -55,6 +51,7 @@ function advance(dt, n, bodies = SYSTEM, pairs = PAIRS)
             mag = dt*((dx*dx + dy*dy) + dz*dz)^-1.5
             b1m = m1*mag
             b2m = m2*mag
+
             v1[1] -= dx*b2m
             v1[2] -= dy*b2m
             v1[3] -= dz*b2m
@@ -76,7 +73,7 @@ function advance(dt, n, bodies = SYSTEM, pairs = PAIRS)
     end
 end
 
-function report_energy(bodies = SYSTEM, pairs = PAIRS, e = 0.0)
+function report_energy(bodies = BODIES, pairs = PAIRS, e = 0.0)
     for ((a1,v1,m1),(a2,v2,m2)) in pairs
         # (((x1, y1, z1), v1, m1), ((x2, y2, z2), v2, m2))
         # Converted
@@ -101,7 +98,7 @@ function report_energy(bodies = SYSTEM, pairs = PAIRS, e = 0.0)
     println(e);
 end
 
-function offset_momentum(ref, bodies = SYSTEM, px = 0.0, py = 0.0, pz = 0.0)
+function offset_momentum(ref, bodies = BODIES, px = 0.0, py = 0.0, pz = 0.0)
     for (r,v,m) in bodies
         # (r, [vx, vy, vz], m)
         # Converted
@@ -119,8 +116,8 @@ function offset_momentum(ref, bodies = SYSTEM, px = 0.0, py = 0.0, pz = 0.0)
     v[3] = (pz/m)
 end
 
-function main_func(n, ref = "sun")
-    offset_momentum(BODIES[ref]);
+function main_func(n)
+    offset_momentum(BODIES[1]);
     report_energy();
     advance(0.01, n);
     report_energy();
