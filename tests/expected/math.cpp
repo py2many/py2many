b6fbc2fa81c643c5cfcfec1d0cd71279efe81c62
John Vandenberg <jayvdb@gmail.com>
2022-06-08 21:00:49 +0800

    Add math test case

---
+++ tests/expected/math.cpp
@@ -0,0 +1,14 @@
+#include <iostream>  // NOLINT(build/include_order)
+
+#include "pycpp/runtime/builtins.h"  // NOLINT(build/include_order)
+#include "pycpp/runtime/sys.h"       // NOLINT(build/include_order)
+inline void main_func() {
+  int a = std::pow(2, 4);
+  std::cout << a;
+  std::cout << std::endl;
+}
+
+int main(int argc, char** argv) {
+  pycpp::sys::argv = std::vector<std::string>(argv, argv + argc);
+  main_func();
+}
---
+++ tests/expected/math.jl
@@ -0,0 +1,10 @@
+function main_func()
+    a = 2^4
+    println(join([a], " "))
+end
+
+function main()
+    main_func()
+end
+
+main()
