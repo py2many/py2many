a6b6b354c88ccb53fcbab715983a5e10426b933e
MiguelMarcelino <mac.marcelino@outlook.pt>
2022-06-09 18:36:51 +0100

    Add new test cases

---
+++ tests/cases/newman_conway_sequence.py
@@ -0,0 +1,13 @@
+''' Python program to find the n-th element of
+    Newman-Conway Sequence'''
+
+def sequence(n: int):
+    f = [0, 1, 1]
+    for i in range(3, n + 1):
+        r = f[f[i-1]]+f[i-f[i-1]]
+        f.append(r)
+    return r
+     
+if __name__ == '__main__':
+    n = 10
+    print(sequence(n))
