1a3587fd3be6994a852e6fa05c1458acb933ee88
Arun Sharma <arun@sharma-home.net>
2024-08-04 11:08:56 -0700

    Run black

--- pyd/__init__.py
+++ pyd/__init__.py
@@ -6,7 +6,7 @@ from .transpiler import DIntegerDivRewriter, DTranspiler
 
 
 def settings(args, env=os.environ):
-    dfmt_args = [ "--inplace", "--brace_style=otbs" ]
+    dfmt_args = ["--inplace", "--brace_style=otbs"]
     if args.indent is not None:
         dfmt_args.append(f"--indent_size:{args.indent}")
     return LanguageSettings(
