f3b5e19e9eda3c86b5957bdd5aa0a571af7ca364
Arun Sharma <arun@sharma-home.net>
2024-09-07 10:57:00 -0700

    Test fixes

--- py2many/raises_transformer.py
+++ py2many/raises_transformer.py
@@ -10,7 +10,7 @@ class RaisesTransformer(ast.NodeTransformer):
     Annotates function def ast nodes with information about exceptions
     """
 
-    def visit_Assert(self, node):
+    def _mark_parent_raises(self, node):
         fndef = None
         for scope in node.scopes:
             if isinstance(scope, ast.FunctionDef):
@@ -18,5 +18,16 @@ class RaisesTransformer(ast.NodeTransformer):
                 break
         if fndef is not None:
             fndef.raises = True
+
+    def visit_Assert(self, node):
+        self._mark_parent_raises(node)
+        self.generic_visit(node)
+        return node
+
+    def visit_Call(self, node) -> str:
+        fname = self.visit(node.func)
+        fndef = node.scopes.find(fname)
+        if fndef is not None and hasattr(fndef, "raises"):
+            self._mark_parent_raises(node)
         self.generic_visit(node)
         return node
