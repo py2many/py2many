547a999742e73a50b959dd89c7c1f4c8e4434f84
Arun Sharma <arun@sharma-home.net>
2024-12-27 21:55:59 -0800

    update cppitertools version

--- scripts/cpp-setup.sh
+++ scripts/cpp-setup.sh
@@ -1,2 +1,2 @@
-conan download cppitertools/2.1@:5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9 -r conancenter
+conan download cppitertools/2.1@:38e2c3e8f61d869caa1d66b64a7bc0d5 -r conancenter
 conan download catch2/3.5.0@:96438105cb542059596be5ff639e510c5c525967 -r conancenter
