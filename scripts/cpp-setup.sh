e7e3243a4a8abedf0cf67d6633ad8d887287834d
Arun Sharma <arun@sharma-home.net>
2024-12-27 22:00:38 -0800

    Use the packageid for cppitertools

--- scripts/cpp-setup.sh
+++ scripts/cpp-setup.sh
@@ -1,2 +1,2 @@
-conan download cppitertools/2.1@:38e2c3e8f61d869caa1d66b64a7bc0d5 -r conancenter
+conan download cppitertools/2.1@:da39a3ee5e6b4b0d3255bfef95601890afd80709 -r conancenter
 conan download catch2/3.5.0@:96438105cb542059596be5ff639e510c5c525967 -r conancenter
