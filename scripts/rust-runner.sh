b2074c73367f547473d7f23a30b50747f1578c21
Arun Sharma <arun@sharma-home.net>
2025-01-31 16:20:34 -0800

    rust: improve devx

---
+++ scripts/rust-runner.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+MODE=$1
+shift
+
+
+# Determine the correct sed command
+if [[ "$(uname)" == "Darwin" ]]; then
+    # macOS: Use gsed
+    SED="gsed"
+else
+    # Linux/Other: Use sed
+    SED="sed"
+fi
+
+# Define the directory path
+DIR="common-rust-proj"
+
+# Check if the directory exists
+if [ ! -d "$DIR" ]; then
+    # If the directory doesn't exist, create a new Cargo library project
+    cargo new --bin "$DIR"
+
+    echo "Created new Cargo binary project in $DIR"
+fi
+
+# Extract the embedded Cargo.toml content
+# Look for lines between ```cargo and ```
+# Remove the first 4 characters (e.g., "//! ") from each line
+$SED -n '/```cargo/,/```/p' "$1" | $SED '1d;$d' | $SED 's/^....//' > $DIR/Cargo.toml
+bin_name=$(basename -s .rs $1)
+$SED -i "s/^.package.\$/[package]\nname=\"$bin_name\"/" $DIR/Cargo.toml
+
+# Now copy the argument to the target dir
+cp $1 $DIR/src/main.rs
+shift;
+
+if [ "$MODE" = "lint" ]; then
+    cd $DIR
+    cargo clippy --fix --allow-dirty
+    cargo clippy
+else
+    cargo run --manifest-path $DIR/Cargo.toml $*
+fi
