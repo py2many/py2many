76a0e335129769409163680e1c5a60e7b5f1390e
Arun Sharma <arun@sharma-home.net>
2026-02-18 22:44:10 -0800

    ast: revert changes to rename py_ast

--- pyv/clike.py
+++ pyv/clike.py
@@ -1,5 +1,4 @@
 import ast
-import ast as py_ast
 from typing import Dict, Set
 
 from py2many.clike import CLikeTranspiler as CommonCLikeTranspiler
@@ -58,35 +57,35 @@ v_keywords: Set[str] = frozenset(
 )
 
 v_symbols: Dict[type, str] = {
-    py_ast.Eq: "==",
-    py_ast.Is: "==",
-    py_ast.NotEq: "!=",
-    py_ast.Pass: "",
-    py_ast.Mult: "*",
-    py_ast.Add: "+",
-    py_ast.Sub: "-",
-    py_ast.Div: "/",
-    py_ast.FloorDiv: "/",
-    py_ast.Mod: "%",
-    py_ast.Lt: "<",
-    py_ast.Gt: ">",
-    py_ast.GtE: ">=",
-    py_ast.LtE: "<=",
-    py_ast.LShift: "<<",
-    py_ast.RShift: ">>",
-    py_ast.BitXor: "^",
-    py_ast.BitOr: "|",
-    py_ast.BitAnd: "&",
-    py_ast.Not: "!",
-    py_ast.IsNot: "!=",
-    py_ast.USub: "-",
-    py_ast.And: "&&",
-    py_ast.Or: "||",
-    py_ast.In: "in",
+    ast.Eq: "==",
+    ast.Is: "==",
+    ast.NotEq: "!=",
+    ast.Pass: "",
+    ast.Mult: "*",
+    ast.Add: "+",
+    ast.Sub: "-",
+    ast.Div: "/",
+    ast.FloorDiv: "/",
+    ast.Mod: "%",
+    ast.Lt: "<",
+    ast.Gt: ">",
+    ast.GtE: ">=",
+    ast.LtE: "<=",
+    ast.LShift: "<<",
+    ast.RShift: ">>",
+    ast.BitXor: "^",
+    ast.BitOr: "|",
+    ast.BitAnd: "&",
+    ast.Not: "!",
+    ast.IsNot: "!=",
+    ast.USub: "-",
+    ast.And: "&&",
+    ast.Or: "||",
+    ast.In: "in",
 }
 
 
-def v_symbol(node: py_ast.AST) -> str:
+def v_symbol(node: ast.AST) -> str:
     """Find the equivalent C symbol for a Python ast symbol node"""
     symbol_type = type(node)
     return v_symbols[symbol_type]
@@ -99,14 +98,14 @@ class CLikeTranspiler(CommonCLikeTranspiler):
         CommonCLikeTranspiler._container_type_map = V_CONTAINER_TYPE_MAP
         self._statement_separator: str = ""
 
-    def visit(self, node: py_ast.AST) -> str:
+    def visit(self, node: ast.AST) -> str:
         if type(node) in v_symbols:
             return v_symbol(node)
         else:
             return super().visit(node)
 
-    def visit_BinOp(self, node: py_ast.BinOp) -> str:
-        if isinstance(node.op, py_ast.Pow):
+    def visit_BinOp(self, node: ast.BinOp) -> str:
+        if isinstance(node.op, ast.Pow):
             left: str = self.visit(node.left)
             right: str = self.visit(node.right)
             self._usings.add("math")
@@ -122,7 +121,7 @@ class CLikeTranspiler(CommonCLikeTranspiler):
         left_rank: int = V_WIDTH_RANK.get(left_type, -1)
         right_rank: int = V_WIDTH_RANK.get(right_type, -1)
 
-        if isinstance(node.op, py_ast.Mult):
+        if isinstance(node.op, ast.Mult):
             if left_type == "string" and right_type == "int":
                 return f"({left}.repeat({right}))"
             if left_type.startswith("[]") and right_type == "int":
@@ -146,7 +145,7 @@ class CLikeTranspiler(CommonCLikeTranspiler):
             op = {"&": "&&", "|": "||", "^": "!="}.get(op, op)
         return f"({left} {op} {right})"
 
-    def visit_Name(self, node: py_ast.Name) -> str:
+    def visit_Name(self, node: ast.Name) -> str:
         try:
             if node.id in v_keywords:
                 return f"@{node.id}"
@@ -157,7 +156,7 @@ class CLikeTranspiler(CommonCLikeTranspiler):
             traceback.print_exc()
             raise
 
-    def visit_In(self, node: py_ast.Compare) -> str:
+    def visit_In(self, node: ast.Compare) -> str:
         left: str = self.visit(node.left)
         right: str = self.visit(node.comparators[0])
         return f"{left} in {right}"
