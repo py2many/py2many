4dbb582854cf13296bfc7c9af5b79eadbb0230d7
Arun Sharma <arun@sharma-home.net>
2026-05-24 20:45:40 -0700

    Make Julia formatter setup compatible with 1.10

--- scripts/julia-setup.sh
+++ scripts/julia-setup.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
+set -e
 
 julia -e 'using Pkg; Pkg.add("JuliaFormatter"); Pkg.add("SuperEnum")'
 
-julia -e 'using Pkg; Pkg.Apps.add("JuliaFormatter")'
+if julia -e 'using Pkg; exit(isdefined(Pkg, :Apps) ? 0 : 1)'; then
+  julia -e 'using Pkg; Pkg.Apps.add("JuliaFormatter")'
+else
+  cat >/usr/local/bin/jlfmt <<'EOF'
+#!/usr/bin/env julia
+using JuliaFormatter
+JuliaFormatter.main(ARGS)
+EOF
+  chmod +x /usr/local/bin/jlfmt
+fi
